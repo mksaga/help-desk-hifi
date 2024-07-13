198ace5485676913cf16e27e19e1b14c4eaed4f7
Mohamed Aly <mohamedaly@monzo.com>
2024-07-12 20:13:01 -0400

    Fix router

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + za TypeScript Server');
 });
 
-app.use('/tickets', require('./tickets/router.ts'));
+app.use('/tickets', require('./tickets/router'));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
