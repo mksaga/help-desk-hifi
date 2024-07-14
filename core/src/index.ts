838b0a941132ffe0d3b740df0c2f50fc0dc25cc7
Mohamed Aly <mohamedaly@monzo.com>
2024-07-14 16:08:43 -0400

    Configure CORS url

--- core/src/index.ts
+++ core/src/index.ts
@@ -9,6 +9,13 @@ const cors = require("cors")
 const port = process.env.PORT || 3000;
 
 app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://help-desk-hifi.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use('/tickets', require('./tickets/router'));
